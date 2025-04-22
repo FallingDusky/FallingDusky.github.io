e1292bd85f3c29310e6ba9b5b2e725d0a1c0aa4c
FallingDusky <507559351@qq.com>
2025-04-22 13:33:07 +0800

    Site updated: 2025-04-22 13:33:01

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1744809104245";
+const VERSION = "1745299979196";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
